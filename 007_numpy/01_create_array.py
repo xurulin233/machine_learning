4df362e5c9402d1eb451372f04dea0bafa368007
xurulin <804362478@qq.com>
2024-05-07 10:42:30 +0800

    add array manipulation and operations

--- 007_numpy/01_create_array.py
+++ 007_numpy/01_create_array.py
@@ -29,3 +29,36 @@ linspace_array = np.linspace(0, 1, 5)
 print(linspace_array)
 
 #这个初始化功能在数据可视化、科学计算的时候非常有用，一般用来给自变量x取样
+
+# 改变数组形状,重塑操作不改变数组中的元素总数，只是改变其布局。
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+reshaped = arr.reshape(3, 2)
+print(reshaped)
+
+# 水平拼接数组
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+concatenated = np.hstack((arr1, arr2))
+print(concatenated)
+
+'''
+这个操作在你需要将来自不同数据源的数据按列合并时非常有用，如合并两个数据集的特征。
+'''
+
+# 垂直拼接数组
+concatenated = np.vstack((arr1, arr2))
+print(concatenated)
+
+
+A = np.array([[3, 1], [1, 2]])
+b = np.array([9, 8])
+x = np.linalg.solve(A, b)
+print(x)  # 输出解向量
+
+
+A = np.array([[1, 2], [3, 4]])
+eigenvalues, eigenvectors = np.linalg.eig(A)
+print(eigenvalues)  # 特征值
+print(eigenvectors)  # 特征向量
+
+
