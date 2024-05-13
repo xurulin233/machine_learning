cb5ee3be07014de9b1734b4980cad002fa8ed64e
xurulin <804362478@qq.com>
2024-05-13 15:56:35 +0800

    add Image color quantization

---
+++ 016_k-means/paojie.py
@@ -0,0 +1,72 @@
+import cv2 as cv 
+import numpy as np
+import matplotlib.pyplot as plt
+from glob import glob
+
+def k_means(img, Class=5):
+    # get shape
+    H, W, C = img.shape
+
+    # initiate random seed
+    np.random.seed(0)
+
+    # reshape image
+    img = np.reshape(img, (H * W, -1))
+
+    # get index randomly
+    i = np.random.choice(np.arange(H * W), Class, replace=False)
+    Cs = img[i].copy()
+    print(Cs)
+
+    while True:
+        # prepare pixel class label
+        clss = np.zeros((H * W), dtype=int)
+        
+        # each pixel
+        for i in range(H * W):
+            # get distance from index pixel
+            dis = np.sqrt(np.sum((Cs - img[i])**2, axis=1))
+            # get argmin distance
+            clss[i] = np.argmin(dis)
+
+        # selected pixel values
+        Cs_tmp = np.zeros((Class, 3))
+        
+        # each class label
+        for i in range(Class):
+            Cs_tmp[i] = np.mean(img[clss == i], axis=0)
+
+        # if not any change
+        if (Cs == Cs_tmp).all():
+            break
+        else:
+            Cs = Cs_tmp.copy()
+
+    # prepare out image
+    out = np.zeros((H * W, 3), dtype=np.float32)
+
+    # assign selected pixel values  
+    for i in range(Class):
+        out[clss == i] = Cs[i]
+
+    print(Cs)
+        
+    out = np.clip(out, 0, 255)
+
+    # reshape out image
+    out = np.reshape(out, (H, W, 3))
+    out = out.astype(np.uint8)
+
+    return out
+
+# read image
+img = cv.imread("./paojie.jpg").astype(np.float32)
+
+# K-means
+out = k_means(img,Class=3)
+
+cv.imwrite("out.jpg", out)
+cv.imshow("result", out)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
